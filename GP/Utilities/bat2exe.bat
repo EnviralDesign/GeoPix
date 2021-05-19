41753d522c7a6671679394304b481e6230aa969c
LUCAS M MORGAN <enviraldesign@gmail.com>
2021-05-19 15:05:33 -0500

    updated to TD version 13610, added GeoPix.exe launcher
    1) some updates to ctrl+s toe build procedure. Can now append/skip log step, can update patrons if desired, and rebuild the new GeoPix.exe launcher, that is to be used in place of the .bat launcher.

---
+++ GP/Utilities/bat2exe.bat
@@ -0,0 +1,8 @@
+"C:\Program Files\Bat To Exe Converter\Bat_To_Exe_Converter.exe" ^
+/bat "C:\Users\envir\Documents\GitHub\GeoPix\START_GEOPIX.bat" ^
+/exe "C:\Users\envir\Documents\GitHub\GeoPix\GeoPix.exe" ^
+/icon "C:\Users\envir\Documents\GitHub\GeoPix\GP\Assets\customIcons\geopix_icon.ico" ^
+/overwrite ^
+/fileversion 2.0.15255.0 ^
+/productname "GeoPix" ^
+/company "Enviral Design"
